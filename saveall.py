37edb2cad53622649d2af7847f5a00f7a87287b1
Uzair <uhass924@gmail.com>
2024-12-18 21:39:56 -0500

    revamped saveall

--- saveall.py
+++ saveall.py
@@ -1,11 +1,38 @@
-import sys, os, restoreall, time, constants
+import sys, os, restoreall, time, constants, shutil
 
 
 def save_settings(app_name: str):
     HOME = os.getenv("HOME")
-    target_path = constants.get_target_path(app_name).split("/")
-    source_path = os.path.join(HOME, "Library", target_path, f"com.apple.{app_name}.plist")
-    # source_path = os.path.join(
+    single_flag = False
+    source_path = constants.get_target_path(app_name).split("/")
+    match source_path[1]:
+        case "Containers":
+            target_path = os.path.join(HOME, "plists", app_name, "Data")
+        case "Preferences":
+            single_flag = True
+            target_path = os.path.join(HOME, "plists", app_name, source_path[2])
+        case _:
+            raise Exception("Unknown path for the application. Please check constants.")
+    epochtime = int(time.time())
+    backup = os.path.join(
+        HOME, "plists", "backup", app_name, f"{epochtime}", source_path[2]
+    )
+    if not os.path.exists(target_path):
+        os.makedirs(target_path)
+    if not os.path.exists(backup):
+        os.makedirs(backup)
+    if os.path.exists(target_path) and os.path.getsize(target_path) > 0:
+        (
+            shutil.copy2(target_path, backup)
+            if single_flag
+            else restoreall.copy(target_path, backup)
+        )
+    (
+        shutil.copy2(source_path, target_path)
+        if single_flag
+        else restoreall.copy(source_path, target_path)
+    )
+
 
 def run(
     countdown: bool = True,
